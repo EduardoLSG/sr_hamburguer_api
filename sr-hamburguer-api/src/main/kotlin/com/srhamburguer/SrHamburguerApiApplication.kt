21ac7771d9c07a36d35ce6c3dbd1595d6d5a836a
Eduardo Greve <eduardo.greve@semeq.com>
2024-01-16 23:40:51 -0300

    first project

---
+++ sr-hamburguer-api/src/main/kotlin/com/srhamburguer/SrHamburguerApiApplication.kt
@@ -0,0 +1,11 @@
+package com.srhamburguer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SrHamburguerApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<SrHamburguerApiApplication>(*args)
+}
---
+++ sr-hamburguer-api/src/test/kotlin/com/srhamburguer/SrHamburguerApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.srhamburguer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SrHamburguerApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
